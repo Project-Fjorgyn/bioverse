af5b8b0336497aa1fe2403830f433961328c99ce
erebuseternal <marcelsanders96@gmail.com>
2022-03-12 07:58:33 -0500

    updated default link

--- src/context/info.context.js
+++ src/context/info.context.js
@@ -7,9 +7,7 @@ export const InfoContext = createContext();
 export function InfoContextProvider({ children }) {
   const [genus, setGenus] = useState('amelanchier');
   const [species, setSpecies] = useState('laevis');
-  const [iNatLink, setiNatLink] = useState(
-    'https://www.inaturalist.org/taxa/54814-Amelanchier-laevis'
-  );
+  const [iNatLink, setiNatLink] = useState(GetiNatLink('amelanchier', 'laevis'));
   const [wikiLink, setWikiLink] = useState(GetWikiLink('amelanchier', 'laevis'));
 
   const updateSelection = (genus, species) => {
