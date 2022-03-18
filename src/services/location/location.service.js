536e9d5066b169b15af9b499d34e2ac1b4235c70
erebuseternal <marcelsanders96@gmail.com>
2022-03-18 17:21:00 -0400

    pointing to deployed api

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -10,7 +10,7 @@ export function callLocationApi(
 ) {
   var lat = `${nwCorner[0]},${neCorner[0]},${seCorner[0]},${swCorner[0]},${nwCorner[0]}`;
   var lon = `${nwCorner[1]},${neCorner[1]},${seCorner[1]},${swCorner[1]},${nwCorner[1]}`;
-  var uri = `http://127.0.0.1:5000/locations?lat=${lat}&lon=${lon}&zoom=${zoom}&genus=${genus}&species=${species}`;
+  var uri = `https://bioverse-positions.herokuapp.com/locations?lat=${lat}&lon=${lon}&zoom=${zoom}&genus=${genus}&species=${species}`;
   fetch(uri)
     .then((data) => {
       return data.json();
