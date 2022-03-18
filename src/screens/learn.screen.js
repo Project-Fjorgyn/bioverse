a97b0afb55cf7473d3ac09bf781d307ff7071cd2
erebuseternal <marcelsanders96@gmail.com>
2022-03-18 19:06:50 -0400

    safe area for webview

--- src/screens/learn.screen.js
+++ src/screens/learn.screen.js
@@ -2,8 +2,13 @@ import React, { useContext } from 'react';
 import { WebView } from 'react-native-webview';
 
 import { InfoContext } from '../context/info.context';
+import { SafeArea } from '../components/containers.components';
 
 export function LearnScreen() {
   const { wikiLink } = useContext(InfoContext);
-  return <WebView source={{ uri: wikiLink }} />;
+  return (
+    <SafeArea>
+      <WebView source={{ uri: wikiLink }} />
+    </SafeArea>
+  );
 }
